6c08be01d90a547c94dd89d7d834bb80abee441d
MiguelJeronimoOliveira <164900366+MiguelJeronimoOliveira@users.noreply.github.com>
2025-01-17 21:07:19 -0300

    Initial commit

---
+++ src/main/java/com/miguel/jeronimo/CondominioFala/CondominioFalaApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.jeronimo.CondominioFala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondominioFalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondominioFalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel/jeronimo/CondominioFala/CondominioFalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.jeronimo.CondominioFala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondominioFalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
